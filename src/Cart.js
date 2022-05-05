1b9ec2c10b086e6233377d096eafbfc9b999d262
Gyanendra <shahgyanendra123.gmail.com>
2022-05-05 18:07:59 +0530

    prop

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,45 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component{
+    constructor (){
+        super();
+        this.state ={
+        product:[ 
+        {
+            price:9000,
+            title:'Phone',
+            qty:1,
+            img:'',
+            id:1
+        },
+        {
+            price:900,
+            title:'Watch',
+            qty:10,
+            img:'',
+            id:2
+        },
+        {
+            price:900,
+            title:'laptop',
+            qty:1,
+            img:'',
+            id:3
+        }   
+            ]
+        }
+    }
  render(){
+     const {product} = this.state;
      return(
-         <div className="cart">
-             <CartItem/>
-             <CartItem/>
-             <CartItem/>
+         <div className="cart"> 
+         {/* this is called prop which is similar to the arguments in function */}
+             {product.map((product)=>{
+                  return (
+                  <CartItem product={product}
+                   key={product.id}/>
+                  )
+                })}
          </div>
      )
  }
