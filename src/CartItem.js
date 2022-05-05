7008126dabdce8798a270318025ba3f5b0379a3e
Gyanendra <shahgyanendra123.gmail.com>
2022-05-05 17:12:32 +0530

    adding cart component

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,7 +11,36 @@ class CartItem extends React.Component{
         }
     }
     increaseQuantity=()=>{ 
-        console.log('this',this.state);
+        // console.log('this',this.state);
+
+        //setting setState form no 1 if we are not require prev form 
+        // this.setState({
+        //     qty:this.state.qty+1
+        // });
+
+        //setting the setState form no 2 if we require prev state then we will use this form 
+        this.setState((prevState)=>{
+            return{
+                qty:prevState.qty+1
+            } 
+        },()=>{
+            console.log(this.state.qty);
+        })
+
+
+    }
+    decreaseQuantity=()=>{
+        const {qty}=this.state;
+        if(qty===0){
+            return;
+        }
+        
+        //setting of form 2 for decreasing of the quantity
+        this.setState((prevState)=>{
+            return{
+                qty:prevState.qty-1
+            }
+        });
     }
     render(){
         const {price,title,qty} = this.state;
@@ -31,7 +60,12 @@ class CartItem extends React.Component{
                         src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
                         onClick={this.increaseQuantity}
                         />
-                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"/>
+                        <img 
+                        alt="decrease" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+                        onClick={this.decreaseQuantity}
+                        />
                         <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"/>
 
                     </div>
