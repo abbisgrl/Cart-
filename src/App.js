e19e6826fc36368ae6062a1e9b9667d737b1f450
Gyanendra <shahgyanendra123.gmail.com>
2022-05-09 21:28:05 +0530

    improvised the cart total amount

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
           id: 2
         },
         {
-          price: 999,
+          price: 30000,
           title: 'Laptop',
           qty: 1,
           img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
@@ -102,7 +102,7 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
-        <div>TOTAL:{this.getCartTotal()}</div>
+        <div style={ { fontSize: 25,color: 'red' } }>TOTAL :  {this.getCartTotal()}</div>
       </div>
     );
   }
